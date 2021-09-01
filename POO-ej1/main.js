46bc5572fafbaa51a3c08f2c05363ed7cbf082c6
FabriCYG <fabri71203@gmail.com>
2021-09-01 17:03:21 -0300

    Subo POO ej1 :P

---
+++ POO-ej1/main.js
@@ -0,0 +1,29 @@
+import Bhaskara from "./bhaskara.js";
+
+const mostrar = () => {
+
+    const a = document.getElementById("inp_a").value
+    const b = document.getElementById("inp_b").value
+    const c = document.getElementById("inp_c").value
+
+    //1. se crea la instancia
+    //2. se envian los datos al constructor 
+    //3. se ejecuta el constructor
+    const bhaskara = new Bhaskara(a,b,c)
+
+    /*
+    haciendo uso de la instancia, accedo al metodo 
+    calcular raiz para ejecutarlo
+    */
+    const raiz1 = bhaskara.calcular_raiz1()
+    const raiz2 = bhaskara.calcular_raiz2()
+
+    document.getElementById("r1").textContent = "Raiz 1 = "+raiz1;
+    document.getElementById("r2").textContent = "Raiz 2 = "+raiz2;
+
+
+}
+
+const boton = document.getElementById("btn_calcular")
+
+boton.addEventListener("click", mostrar)
